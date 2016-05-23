ab7c124eeb701689b1f437dfa4330d89bb386d3f
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-05-23 16:38:09 +0600

    Update generate_bind_configs.sh

--- generate_bind_configs.sh
+++ generate_bind_configs.sh
@@ -7,6 +7,7 @@ ip=$2
 zones=/etc/named.reductor.zones
 DOMAIN_TMPLT=/opt/named_fakezone_generator/reductor_named_domain.tmplt
 
+# удаляем сгенерированные в прошлый раз зоны, префикс чтобы не трогать чужие зоны при этом
 cleanup() {
 	> $zones
 	mkdir -p /etc/named/
@@ -14,10 +15,12 @@ cleanup() {
 	rm -f $file.processed
 }
 
+# с кириллическими доменами пока что проблема, вообще здесь избавляемся от дублирования из-за fqdn/www.
 process_list() {
 	grep -v [а-я] | sed 's/\.$//' | sed -e 's/^www\.//' | sort -u
 }
 
+# генерируем всё необходимое для блокировки одного конкретного домена
 create_config() {
 	local domain=$1
 	local ip=$2
@@ -35,6 +38,8 @@ main() {
 	while read domain; do
 		create_config $domain $ip
 	done < $file.processed
+	# стараемся залить данные "мягко", но сервер может лежать, тогда поднимаем его
+	# вообще спорный момент, в теории можно убрать || service named restart
 	rndc reload || service named restart
 }
 
